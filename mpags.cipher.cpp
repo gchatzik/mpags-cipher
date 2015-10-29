cf8b46a4c7d60f3a9cf5c39e25a6e21f7af16438
gchatzik <georgios.chatzikonstantinidis@gmail.com>
2015-10-29 17:29:57 +0000

    function that converts low to upper letters and numbers (char) to string numbers

--- mpags.cipher.cpp
+++ mpags.cipher.cpp
@@ -1,44 +1,61 @@
 #include <iostream>
 #include <string>
+#include <typeinfo>
 
 
-int main()
+void transformChar(char input)
 {
-  // initialize int                                             
-  int var{3};
-  std::cout<<"Inital value of int variable is : "<< var << std::endl;
- 
-  // modify int                                             
-  var = 4;
-  std::cout<<"New value of int variable is : "<< var << std::endl;
- 
-  // const double                                              
-  const double c{5};
-  std::cout<<"The value of the constand double type of parameter is : "<< c <<std::endl;
- 
-  // const double * int                                             
-  std::cout<<"The product of the previous parameters is : "<<c*var<<std::endl;
-  
-  // auto                                             
-  auto f{true};
 
-  // double/int
-  double r1{2.1};
-  int r2{2};
-  std::cout<<"Devide double with int : "<< r1/r2 << std::endl;
+  //Definee two string with Up and Low letters used to check the input of the user   
+  //Have the same order in char between the low and upper string                                                                                                                                                          
 
-  // int/int
-  int r3{2};
-  int r4{4};
-  std::cout<<"Devide int with int : "<< r3/r4 << std::endl;
+  std::string lowlet{"abcdefghijklmnopqrstuvwxyz"};
+  std::string Upplet{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+  std::string Number{"0123456789"};
+  std::string map[10] = {"ZERO","ONE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NIGN"};
 
-  // string
-  std::string msg{"Hello!"};
-  std::cout<<"Initialize string : " << msg << std::endl;
+  //for upper
+  for (int i{0}; i < 26; i++ )
+    {
+      if (input != Upplet[i]) continue;
+      else
+	{
+	  std::cout<<input<<std::endl;
+	  return;
+	}
+    }
 
-  // asign to auto the first character of the string defined before                                             
-  auto sg{msg[0]};
+  //for low
+  for (int i{0}; i < 26; i++)
+    {
+      if (input != lowlet[i]) continue;
+      else {
+	std::cout<<Upplet[i]<<std::endl;
+	return;
+      }
+    }
+
+  //for number                                                                                                                                                                                                            
+  for (int i{0}; i < 10; i++ )
+    {
+      if (input != Number[i]) continue;
+      else {
+	std::cout<< map[i]<<std::endl;
+	return;
+      }
+    }
+
+}
+
+int main(){
+
+
+  char input{'x'};
+  while(std::cin>>input)
+    {
+      transformChar(input);
+    }
+
 
   return 0;
 }
-
